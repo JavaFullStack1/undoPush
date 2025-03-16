ece78917b110cc0c2ffe0c6aab72f4b0c883adbc
santoshluhar <santoshvtucse@gmail.com>
2025-03-16 11:34:56 +0530

    first commit

---
+++ src/main/java/com/undoPush/UndoPushApplication.java
@@ -0,0 +1,13 @@
+package com.undoPush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndoPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndoPushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/undoPush/UndoPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undoPush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndoPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
